a698b0b5f1b8e4526df8710973e38ac354db3c0a
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2022-12-05 15:39:32 +0200

    2

--- src/api.js
+++ src/api.js
@@ -1,29 +1,26 @@
 const axios = require('axios').default;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const KEY = '31788075-b7615e81e6dda1dedffa3dd10';
+
 export default class PixabayApi {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
 
-  fetchPictures() {
-    // try {
-    //   const { data } = await axios.get(
-    //     `https://pixabay.com/api/?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
-    //   );
-    // } catch (error) {
-    //   console.error(error);
-    // }
-
-    return fetch(
+  async fetchPictures() {
+    const { data } = await axios.get(
       `https://pixabay.com/api/?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
-    )
-      .then(resp => resp.json())
-      .then(data => {
-        this.page += 1;
+    );
 
-        return data;
-      });
+    if (Math.ceil(data.totalHits / 40) < this.page) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+
+    this.page += 1;
+
+    return data;
   }
 
   restPage() {
@@ -36,5 +33,9 @@ export default class PixabayApi {
 
   set query(newQuery) {
     this.searchQuery = newQuery;
+  }
+
+  get amountOfPage() {
+    return this.page;
   }
 }
